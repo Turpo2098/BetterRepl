b0ae8ba0b7710c4665b3d0aab0e565568dc030b4
Turpo2098 <thilo2002@gmx.de>
2023-10-29 09:43:46 +0100

    Added that no items get removed when the player is in creative Mode

--- src/main/java/tf/tfischer/betterrepl/listener/ReplUsage.java
+++ src/main/java/tf/tfischer/betterrepl/listener/ReplUsage.java
@@ -142,6 +142,9 @@ public class ReplUsage implements Listener {
     }
 
     private void removeOneItem(Player player, Material material){
+        if(player.getGameMode().equals(GameMode.CREATIVE))
+            return;
+
         for(ItemStack item : player.getInventory().getContents()){
             System.out.println("Betterrepl: " + item);
             if(item == null)
